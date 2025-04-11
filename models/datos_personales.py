4fcb2a7ed4f0c5d745e2fd0c1d6bf69d2210fafd
Yildy <segura.aguirre.21118@itsmante.edu.mx>
2025-04-11 13:51:16 -0600

    Agrega datosPersonales

---
+++ models/datos_personales.py
@@ -0,0 +1,12 @@
+from datetime import date
+from config import db
+
+class DatosPersonales(db.Model):
+    __tablename__ = 'datos_personales'
+
+    usuario_id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    telefono = db.Column(db.String(20))
+    fecha_nacimiento = db.Column(db.Date)
+
